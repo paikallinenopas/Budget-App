af268c3f2cffe273694100ddc7b33eda21cf010e
paikallinenopas <iina.kaipinen@outlook.com>
2026-07-26 20:26:47 +0000

    Saldot ja funktiot

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 let saldo = 0;
 let tulot = 0;
 let menot = 0;
+let tallennetut = JSON.parse(localStorage.getItem("budgetData"));
 
+if (tallennetut) {
+    saldo = tallennetut.saldo;
+    tulot = tallennetut.tulot;
+    menot = tallennetut.menot;
+}
 const tulotElementti = document.getElementById("tulot");
 const menotElementti = document.getElementById("menot");
 const saldoElementti = document.getElementById("saldo");
 const tapahtumat = document.getElementById("tapahtumat");
 
+function tallennaTiedot() {
+    localStorage.setItem("budgetData", JSON.stringify({
+        saldo: saldo,
+        tulot: tulot,
+        menot: menot
+    }));
+}
+
 function paivitaSaldo() {
     saldoElementti.textContent = saldo + " €";
     tulotElementti.textContent = tulot + " €";
     menotElementti.textContent = menot + " €";
+
+    tallennaTiedot();
+}
 }
 function lisaaTulo() {
     let summa = Number(prompt("Anna tulon määrä (€):"));
