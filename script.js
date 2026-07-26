8449828f2944fc3bc061b3540d63ff8d60befaa3
paikallinenopas <iina.kaipinen@outlook.com>
2026-07-26 17:54:14 +0000

    Lisätty saldo ja toiminnot

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert("Tervetuloa Budget Appiin!");
+let saldo = 0;
+
+function paivitaSaldo() {
+    document.getElementById("saldo").textContent = saldo + " €";
+}
+
+function lisaaTulo() {
+    let summa = Number(prompt("Anna tulon määrä (€):"));
+
+    if (!summa) return;
+
+    saldo += summa;
+
+    let lista = document.getElementById("tapahtumat");
+    lista.innerHTML += `<li>💵 Tulo +${summa} €</li>`;
+
+    paivitaSaldo();
+}
+
+function lisaaMeno() {
+    let summa = Number(prompt("Anna menon määrä (€):"));
+
+    if (!summa) return;
+
+    saldo -= summa;
+
+    let lista = document.getElementById("tapahtumat");
+    lista.innerHTML += `<li>💸 Meno -${summa} €</li>`;
+
+    paivitaSaldo();
+}
