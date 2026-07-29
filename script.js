c4904b44eb6b2da204be3657683bc28cf582733a
paikallinenopas <iina.kaipinen@outlook.com>
2026-07-29 22:18:16 +0000

    url

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ FINERO 3.0
 console.log("script.js ladattu");
 
 const SUPABASE_URL = "https://aaalfykbovslexndompa.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_G7z8XDhx1mZpeOSGqk9OWw_lWhXj0y6";
+const SUPABASE_ANON_KEY = "sb_publishable_G7z8XDhx1mZpeOSGqk9ONw_lWHxJ0y6";
 
 const sb = window.supabase.createClient(
     SUPABASE_URL,
